53ee4a629d67d5567c5af22eed2bfc73c0a9852c
Darrell Payne <darrellpayne@earthlink.net>
2019-05-01 18:17:47 -0700

    21. App Info Auto Sizing Cells

--- AppStoreJSONApis/Controllers/Apps/Details/AppsDetailController.swift
+++ AppStoreJSONApis/Controllers/Apps/Details/AppsDetailController.swift
@@ -14,13 +14,22 @@ import UIKit
     
     var appId: String! {
         didSet {
-            print("Here is my appId:", appId)
+//            print("Here is my appId:", appId)
             let urlString = "https://itunes.apple.com/lookup?id=\(appId ?? "")"
             Service.shared.fetchGenericJSONData(urlString: urlString) { (result: SearchResult?, err) in
-                print(result?.results.first?.releaseNotes)
+                
+                let app = result?.results.first
+                self.app = app
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+                
+//                print(result?.results.first?.releaseNotes)
             }
         }
     }
+    
+    var app: Result?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,10 +47,22 @@ import UIKit
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellID, for: indexPath) as! AppDetailCell
         
+        cell.app = app
+
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 300)
+        // Calculate the necessary size for our cell somehow
+        let dummyCell = AppDetailCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000))
+        
+        dummyCell.app = app
+        dummyCell.layoutIfNeeded()
+        
+        let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+        
+        
+        
+        return .init(width: view.frame.width, height: estimatedSize.height)
     }
  }
